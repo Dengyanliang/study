e4b1eb9467368d8b0e461679d321658005814f18
yanliangdeng <dengyanliang@meituan.com>
2021-02-02 10:13:18 +0800

    算法学习

--- my-study/src/main/java/com/deng/study/algorithm/linear/MyQueue2.java
+++ my-study/src/main/java/com/deng/study/algorithm/linear/MyQueue2.java
@@ -3,24 +3,24 @@ package com.deng.study.algorithm.linear;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * @Desc:队列操作：有问题，元素出队后，不能被重复利用
+ * @Desc:队列操作
  * @Auther: dengyanliang
  * @Date: 2021/1/23 21:27
  */
 @Slf4j
-public class MyQueue {
+public class MyQueue2 {
     private int[] array;
     private int maxSize;
     // 有两个指针
-    private int front = -1; // 指向队列头的前一个位置
-    private int rear = -1;  // 指向队列尾
+    private int front = 0; // 指向队列头的前一个位置
+    private int rear = 0;  // 指向队列尾
     private int size;
 
-    public MyQueue(){
+    public MyQueue2(){
         array = new int[0];
         maxSize = 0;
     }
-    public MyQueue(int size){
+    public MyQueue2(int size){
         array = new int[size];
         this.maxSize = size;
     }
@@ -34,22 +34,28 @@ public class MyQueue {
             log.info("queue is full");
             return;
         }
-        rear++;
         array[rear] = element;
+        log.info("push array["+rear+"]=" +element+ " success");
+
+        rear = (rear + 1) % maxSize;
         size++;
     }
 
     public int poll(){
         checkIsEmpty();
-        front++;
         int element = array[front];
+        front = (front + 1) % maxSize;
         size--;
         return element;
     }
 
     public int peek(){
         checkIsEmpty();
-        return array[front+1]; // 注意要加1
+        return array[front]; // 注意要加1
+    }
+
+    public int getSize(){
+        return (rear + maxSize - front) % maxSize;
     }
 
     private void checkIsEmpty(){
@@ -59,42 +65,51 @@ public class MyQueue {
     }
 
     private boolean checkIsFull(){
-        return rear == maxSize - 1;
+        return (rear + 1 ) % maxSize == front;
     }
 
     public void show(){
         checkIsEmpty();
         log.info("array elements :");
-        for(int i = front + 1; i <= rear; i++){ // 这里要注意指针 front 和 rear
-            System.out.print(array[i] + " ");
+        for(int i = front; i < front + getSize(); i++){ // 这里要注意指针 front 和 rear
+            System.out.print("array["+i % maxSize+"] = "+ array[i % maxSize] + " ");
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
-        MyQueue myQueue = new MyQueue(5);
+        MyQueue2 myQueue = new MyQueue2(5);
         myQueue.add(9);
         myQueue.add(3);
         myQueue.add(10);
         myQueue.add(13);
-        myQueue.add(12);
+//        myQueue.add(12);
 
         myQueue.show();
 
         int element = myQueue.poll();
         log.info("element 1:{}",element);
 
+//        element = myQueue.poll();
+//        log.info("element 2:{}",element);
+//
+//        element = myQueue.poll();
+//        log.info("element 3:{}",element);
+
         element = myQueue.poll();
-        log.info("element 2:{}",element);
-        myQueue.poll();
-        myQueue.poll();
-//        myQueue.poll();
-//        myQueue.poll();
+        log.info("element 4:{}",element);
+
+//        element = myQueue.poll();
+//        log.info("element 5:{}",element);
 
         myQueue.show();
 
-        System.out.println(myQueue.peek());
+//        System.out.println(myQueue.peek());
 
         myQueue.add(100);
+        myQueue.add(200);
+        myQueue.add(300);
+
+        myQueue.show();
     }
 }
--- my-study/src/main/java/com/deng/study/algorithm/linear/MyStack.java
+++ my-study/src/main/java/com/deng/study/algorithm/linear/MyStack.java
@@ -12,64 +12,82 @@ import java.util.Arrays;
 @Slf4j
 public class MyStack {
     private int[] array;
+    private int top = -1; // 栈顶指针
+    private int maxSize;  // 栈的最大长度
+    private int size;
+
     public MyStack(){
         array = new int[0];
+        maxSize = 0;
+    }
+
+    public MyStack(int size){
+        array = new int[size];
+        this.maxSize = size;
     }
 
     public int size(){
-        return array.length;
+        return size;
     }
 
     public void push(int element){
-        // 创建一个新数组，数组长度是原来数组长度+1
-        int[] newArr = new int[array.length+1];
-        // 将原来数组的内容拷贝到新数组中
-        for(int i = 0; i < array.length; i++){
-            newArr[i] = array[i];
+        if(checkIsFull()){
+            System.out.println("stack is full");
+            return;
         }
-        // 在新数组的末尾加入新的元素
-        newArr[array.length] = element;
-        array = newArr;
+        top++;
+        array[top] = element;
+        size++;
+        log.info("push array["+top+"]=" +element+ " success");
     }
 
     public int pop(){
         checkIsEmpty();
-        int element = array[array.length-1];
-
-        int[] newArr = new int[array.length-1];
-        for(int i = 0 ; i < array.length-1; i++){
-            newArr[i] = array[i];
-        }
-        array = newArr;
+        int element = array[top];
+        top--;
+        size--;
         return element;
     }
 
+    // 栈是先进后出的，所以一定要从栈顶开始打印，也就是从数组末尾进行打印
     public void show(){
-        log.info("array elements :{}", Arrays.toString(array));
+        checkIsEmpty();
+        System.out.print("array elements :");
+        for(int i = top; i >= 0; i--){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 
     public int peek(){
         checkIsEmpty();
-        return array[size()-1];
+        return array[top];
     }
 
     private void checkIsEmpty(){
-        if(size() == 0){
+        if(top == -1){
             throw new RuntimeException("stack is empty");
         }
     }
 
+    private boolean checkIsFull(){
+        return top == maxSize - 1;
+    }
+
     public static void main(String[] args) {
-        MyStack myStack = new MyStack();
+        MyStack myStack = new MyStack(5);
         myStack.push(3);
         myStack.push(9);
         myStack.push(5);
+//        myStack.push(4);
+//        myStack.push(10);
+//        myStack.push(2);
 
         myStack.show();
 
         log.info("myStack.pop :{}",myStack.pop());
-//        log.info("myStack.pop :{}",myStack.pop());
-//        log.info("myStack.pop :{}",myStack.pop());
+        log.info("myStack.pop :{}",myStack.pop());
+        log.info("myStack.pop :{}",myStack.pop());
 
 
         log.info("myStack.peek :{}",myStack.peek());
