420eaf2c88229b04efbd433e65d271598f515454
dengyanliang <457718426@qq.com>
2021-04-13 08:33:01 +0800

    java8学习

--- my-study/src/main/java/com/deng/study/java/java8/optional/TestOptional.java
+++ my-study/src/main/java/com/deng/study/java/java8/optional/TestOptional.java
@@ -20,20 +20,16 @@ public class TestOptional {
 
         Optional<Employee> optional = Optional.empty();
         System.out.println("optional:"+optional);
-
+//
         Optional<Employee> optionalEmployee = Optional.of(new Employee());
         System.out.println("optionalEmployee:"+optionalEmployee);
 
-        Optional<Employee> optionalEmployee1 = Optional.ofNullable(new Employee());
+        Optional<Employee> optionalEmployee1 = Optional.ofNullable(new Employee()); // empty 和 of 结合起来
         System.out.println("optionalEmployee1:"+optionalEmployee1);
 
 //        Employee employee1 = optional.get();
 //        Employee employee1 = optional.isPresent();
 //        System.out.println("employee1:"+employee1);
-
-        Employee employee1 = optional.orElse(new Employee("刘备",42)); // orElseGet的区别
-        System.out.println("--employee1:"+employee1);
-
 
 //        Employee employee = optional.orElseThrow(()->new RuntimeException("error"));
 //        System.out.println("===employee:"+employee);
@@ -45,15 +41,16 @@ public class TestOptional {
 
         }
 
-        optionalEmployee.ifPresent((e)->System.out.println(e.getName()));
+        optional.ifPresent((e)->System.out.println(e.getName()+"*****"));
+
+        Employee employee1 = optional.orElse(new Employee("刘备",42)); // orElseGet的区别
+        System.out.println("--employee1:"+employee1);
 
         Employee employee2 = optional.orElseGet(()->new Employee("关羽",40));
-        System.out.println("--employee:"+employee2);
+        System.out.println("--employee2:"+employee2);
 
         optional = Optional.of(employee1);
         String names = optional.filter(e->e.getAge()>30).map(Employee::getName).orElseThrow(()->new RuntimeException("error"));
         System.out.println("names:" + names);
-
-
     }
 }
