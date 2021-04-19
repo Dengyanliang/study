b8caeb1783f287a78461e56cb87faf1841a563d3
邓延良 <QWas^0206>
2021-04-19 15:15:29 +0800

    线程池测试

--- my-study/src/main/java/com/deng/study/java/thread/ThreadTest.java
+++ my-study/src/main/java/com/deng/study/java/thread/ThreadTest.java
@@ -24,10 +24,20 @@ public class ThreadTest {
             threadPoolExecutor = new ThreadPoolExecutor(10,30,30,
                 TimeUnit.SECONDS,new ArrayBlockingQueue<>(500));
         }
-
+        long start = System.currentTimeMillis();
         String name = "zhangsan";
         CompletableFuture<String> future =  CompletableFuture.supplyAsync(() -> print(name), threadPoolExecutor);
-        String result2 = null;
+        System.out.println("---------------------");
+        CompletableFuture<String> future2 =  CompletableFuture.supplyAsync(() -> print2(name), threadPoolExecutor);
+
+        String result = future.join(); // 阻塞式的，所以要放在最后，而不能放在29行后面，这样效率最高
+        System.out.println("result:"+result);
+
+        String result2 = future2.join();
+        System.out.println("result2:" +result2); // 阻塞式的，所以要放在最后
+        long end = System.currentTimeMillis();
+        System.out.println("end-start: " + (end-start)); // 3004
+
         try {
             result2 = future.get(); // 注意和join的区别，需要抛出异常，而join不需要抛出异常
         } catch (InterruptedException e) {
@@ -35,10 +45,9 @@ public class ThreadTest {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        System.out.println(result2);
+        System.out.println("future.get(): "+result2);
 
-        String result = future.join();
-        System.out.println(result);
+
     }
 
     private String print(String s){
@@ -51,4 +60,17 @@ public class ThreadTest {
         }
         return "hello:" + s;
     }
+
+    private String print2(String s){
+        try {
+            System.out.println("sleep begin 2");
+            Thread.sleep(2000);
+            System.out.println("sleep end 2");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "hello2:" + s;
+    }
+
+
 }
