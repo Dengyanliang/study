6a9f395d65525ff2f2a125765861ea50ecd8c07b
邓延良 <QWas^0206>
2021-05-26 11:19:30 +0800

    拷贝数据

--- my-study/src/main/java/com/deng/study/java/CopyTest.java
+++ my-study/src/main/java/com/deng/study/java/CopyTest.java
@@ -32,17 +32,37 @@ public class CopyTest {
         user.setName("zhangsan");
         user.setPetList(petList);
 
+        User user2 = new User();
+        user2.setAge(20);
+        user2.setPersonId(456);
+        user2.setName("lisi");
+        user2.setPetList(petList);
+
         List<User> userList = new ArrayList<>();
         userList.add(user);
+        userList.add(user2);
+
 
 //        User newUser = new User();
 //        List<User> newUserList = new ArrayList<>();
 
-//        BeanUtils.copyProperties(userList,newUserList); // 数据为空
+        Person person3 = new Person();
+        BeanUtils.copyProperties(user2,person3); // 数据为空
+        System.out.println("person3:"+person3);
 
-        List<Person> newUserList = ModelConvertUtil.mapList(userList, Person.class);
-
+        List<Person> newUserList = ModelConvertUtil.copyList(userList, Person.class);
         System.out.println(newUserList);
+
+        System.out.println("**************************");
+
+        Person person = ModelConvertUtil.copyObj(user, Person.class);
+        System.out.println(person);
+
+        System.out.println("------------------");
+
+        Person person2 = new Person();
+        ModelConvertUtil.copyObj(user,person2);
+        System.out.println(person2);
 
     }
 
--- my-study/src/main/java/com/deng/study/util/ModelConvertUtil.java
+++ my-study/src/main/java/com/deng/study/util/ModelConvertUtil.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cglib.beans.BeanCopier;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,83 +17,82 @@ import java.util.stream.Collectors;
  * @author jinxudong
  * @date 2020-01-16 14:51
  */
-public final  class ModelConvertUtil {
+public final class ModelConvertUtil {
     private static final Logger logger = LoggerFactory.getLogger(ModelConvertUtil.class);
 
-    private static final Map<String, BeanCopier> BEAN_COPIERS = new ConcurrentHashMap<String, BeanCopier>();
-    private ModelConvertUtil(){}
+    private static final Map<String, BeanCopier> BEAN_COPIER_MAP = new ConcurrentHashMap<String, BeanCopier>();
+
+    private ModelConvertUtil() {
+    }
 
     private static String genKey(Class<?> srcClazz, Class<?> destClazz) {
         return srcClazz.getName() + destClazz.getName();
     }
-    public static <D,T> List<D> mapList(List<T> srcObj, Class<D> destinationClass) {
-        if(srcObj==null||srcObj.size()==0){
+
+    public static <D, T> List<D> copyList(List<T> srcObj, Class<D> destinationClass) {
+        if (srcObj == null || srcObj.size() == 0) {
             return new ArrayList<>(0);
         }
         try {
             String key = genKey(srcObj.get(0).getClass(), destinationClass);
             BeanCopier copier = null;
-            if (!BEAN_COPIERS.containsKey(key)) {
+            if (!BEAN_COPIER_MAP.containsKey(key)) {
                 copier = BeanCopier.create(srcObj.get(0).getClass(), destinationClass, false);
-                BEAN_COPIERS.put(key, copier);
+                BEAN_COPIER_MAP.put(key, copier);
             } else {
-                copier = BEAN_COPIERS.get(key);
+                copier = BEAN_COPIER_MAP.get(key);
             }
-            BeanCopier copier1=copier;
+            BeanCopier finalCopier = copier;
 
-            List<D> destList= srcObj.stream().map(c->{
+            List<D> destList = srcObj.stream().map(c -> {
                 try {
-                    D destObj=destinationClass.newInstance();
-                    copier1.copy(c, destObj, null);
+                    D destObj = destinationClass.newInstance();
+                    finalCopier.copy(c, destObj, null);
                     return destObj;
-                }catch (Exception ex){
-                    logger.warn("mapList error", ex);
+                } catch (Exception ex) {
+                    logger.warn("copyList error", ex);
                     return null;
-
                 }
             }).collect(Collectors.toList());
             return destList;
-        }
-        catch (Exception ex){
-            logger.warn("mapList error", ex);
+        } catch (Exception ex) {
+            logger.warn("copyList error", ex);
             return new ArrayList<>(0);
         }
     }
 
-    public static void map(Object srcObj, Object destObj)
-    {
-        if(srcObj==null || destObj==null) {
+    public static void copyObj(Object srcObj, Object destObj) {
+        if (srcObj == null || destObj == null) {
             return;
         }
         String key = genKey(srcObj.getClass(), destObj.getClass());
         BeanCopier copier = null;
-        if (!BEAN_COPIERS.containsKey(key)) {
+        if (!BEAN_COPIER_MAP.containsKey(key)) {
             copier = BeanCopier.create(srcObj.getClass(), destObj.getClass(), false);
-            BEAN_COPIERS.put(key, copier);
+            BEAN_COPIER_MAP.put(key, copier);
         } else {
-            copier = BEAN_COPIERS.get(key);
+            copier = BEAN_COPIER_MAP.get(key);
         }
         copier.copy(srcObj, destObj, null);
     }
-    public static <D> D map(Object srcObj, Class<D> destinationClass)
-    {
-        if(srcObj==null || destinationClass == null){
+
+    public static <D> D copyObj(Object srcObj, Class<D> destinationClass) {
+        if (srcObj == null || destinationClass == null) {
             return null;
         }
         String key = genKey(srcObj.getClass(), destinationClass);
         BeanCopier copier = null;
-        if (!BEAN_COPIERS.containsKey(key)) {
+        if (!BEAN_COPIER_MAP.containsKey(key)) {
             copier = BeanCopier.create(srcObj.getClass(), destinationClass, false);
-            BEAN_COPIERS.put(key, copier);
+            BEAN_COPIER_MAP.put(key, copier);
         } else {
-            copier = BEAN_COPIERS.get(key);
+            copier = BEAN_COPIER_MAP.get(key);
         }
-        try
-        {
-            D destObj= destinationClass.newInstance();
+        try {
+            D destObj = destinationClass.newInstance();
             copier.copy(srcObj, destObj, null);
             return destObj;
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return null;
         }
 
