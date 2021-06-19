b876db8ad4a2a5dd81d509dfbe2146de370c89a4
邓延良 <QWas^0206>
2021-06-19 10:28:22 +0800

    rpc netty demo

--- my-study/src/main/java/com/deng/study/rpc/server/NettyRpcEncoder.java
+++ my-study/src/main/java/com/deng/study/rpc/server/NettyRpcEncoder.java
@@ -6,13 +6,13 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
 /**
- * @Desc:
+ * @Desc: 解码器
  * @Auther: dengyanliang
  * @Date: 2021/6/18 21:34
  */
 public class NettyRpcEncoder extends MessageToByteEncoder {
 
-    private Class<?> target;
+    private Class<?> target; // 对目标对象进行解码
 
     public NettyRpcEncoder(Class<?> target) {
         this.target = target;
@@ -21,9 +21,9 @@ public class NettyRpcEncoder extends MessageToByteEncoder {
     @Override
     protected void encode(ChannelHandlerContext context, Object msg, ByteBuf out) throws Exception {
         if(target.isInstance(msg)){
-            byte[] data = JSON.toJSONBytes(msg);
-            out.writeInt(data.length);
-            out.writeBytes(data);
+            byte[] data = JSON.toJSONBytes(msg); // 使用fastjson将对象转化为byte
+            out.writeInt(data.length); // 先将消息长度写入，也就是消息头
+            out.writeBytes(data);  // 消息体中包含要发送的数据
         }
     }
 }
