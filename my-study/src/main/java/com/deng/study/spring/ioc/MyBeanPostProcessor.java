09798c718fe50afadedb3fbc6c46c0438f98a934
邓延良 <QWas^0206>
2021-06-30 11:10:02 +0800

    spring ReflectionUtils demo

--- my-study/src/main/java/com/deng/study/spring/ioc/MyBeanPostProcessor.java
+++ my-study/src/main/java/com/deng/study/spring/ioc/MyBeanPostProcessor.java
@@ -1,8 +1,14 @@
 package com.deng.study.spring.ioc;
 
+import com.deng.study.common.DataSource;
+import com.deng.study.spring.aop.MyAnnotation;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import java.util.Objects;
 
 @Component
 public class MyBeanPostProcessor implements BeanPostProcessor {
@@ -19,7 +25,21 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-//        System.out.println("MyBeanPostProcessor..postProcessAfterInitialization: "+beanName);
-        return null;
+        Class<?> targetClass = AopUtils.getTargetClass(bean);
+        filter(bean,targetClass);
+        return bean;
     }
+
+    private void filter(Object bean,Class<?> targetClass){
+        // 首先得到类上所有方法，针对每一个方法调用MethodFilter进行匹配检查，
+        // 如果匹配上，调用MethodCallback回调方法，该回调方法会递归向上检查所有父类的实现的接口上的所有方法并处理
+        ReflectionUtils.doWithMethods(targetClass,
+                method -> {
+                    System.out.println("MyBeanPostProcessor.filter:"+method.getName());
+                },
+                method -> Objects.nonNull(method.getAnnotation(DataSource.class))
+        );
+    }
+
+
 }
