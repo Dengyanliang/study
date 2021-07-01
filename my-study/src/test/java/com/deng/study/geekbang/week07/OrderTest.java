e01472dcfc11d1c13e4c01490039c751a4ff1bc8
邓延良 <QWas^0206>
2021-07-01 17:50:03 +0800

    juint test

--- my-study/src/test/java/com/deng/study/geekbang/week07/OrderTest.java
+++ my-study/src/test/java/com/deng/study/geekbang/week07/OrderTest.java
@@ -20,6 +20,7 @@ import java.util.concurrent.*;
  * @Auther: dengyanliang
  * @Date: 2021/3/19 00:10
  */
+
 @Slf4j
 // @RunWith(SpringRunner.class)注解的意义在于Test测试类要使用注入的类，比如@Autowired注入的类,)这些类才能实例化到spring容器中，
 // 自动注入才能生效，然直接一个NullPointerExecption
