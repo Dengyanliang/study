f00b16dd14cf4a1e5ab622251c8e495203191aab
邓延良 <QWas^0206>
2021-09-21 22:09:46 +0800

    分布式事务学习

---
+++ distributed-transaction/discover-server/src/main/java/com/deng/dtx/discoverserver/DiscoverServer.java
@@ -0,0 +1,13 @@
+package com.deng.dtx.discoverserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoverServer {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverServer.class);
+    }
+}
