6bc6e2fbcc84cd795fc65fb69db7060dea0054eb
邓延良 <QWas^0206>
2022-01-04 14:19:41 +0800

    kafka 练习

--- my-study/src/main/java/com/deng/study/mq/kafka/KafkaConsumer.java
+++ my-study/src/main/java/com/deng/study/mq/kafka/KafkaConsumer.java
@@ -1,16 +1,15 @@
-package com.deng.study.mq.kafka;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-@Slf4j
-public class KafkaConsumer {
-
-    @KafkaListener(id = "myGroup",topics = "my-test")
-    public void receive(String message){
-      log.info("接收到消息：{}",message);
-    }
-
-}
+//package com.deng.study.mq.kafka;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//public class KafkaConsumer {
+//
+//    @KafkaListener(id = "myGroup",topics = "my-test")
+//    public void receive(String message){
+//      log.info("接收到消息：{}",message);
+//    }
+//}
--- my-study/src/main/java/com/deng/study/mq/kafka/KafkaController.java
+++ my-study/src/main/java/com/deng/study/mq/kafka/KafkaController.java
@@ -1,21 +1,21 @@
-package com.deng.study.mq.kafka;
-
-import com.alibaba.fastjson.JSON;
-import com.deng.study.domain.Pet;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
-
-@RestController
-@RequestMapping("/kafka")
-public class KafkaController {
-
-    @Resource
-    private KafkaProducer kafkaProducer;
-
-    @PostMapping("/send")
-    public String send(@RequestBody Pet pet){
-        kafkaProducer.send(JSON.toJSONString(pet));
-        return "success";
-    }
-}
+//package com.deng.study.mq.kafka;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.deng.study.domain.Pet;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.annotation.Resource;
+//
+//@RestController
+//@RequestMapping("/kafka")
+//public class KafkaController {
+//
+//    @Resource
+//    private KafkaProducer kafkaProducer;
+//
+//    @PostMapping("/send")
+//    public String send(@RequestBody Pet pet){
+//        kafkaProducer.send(JSON.toJSONString(pet));
+//        return "success";
+//    }
+//}
